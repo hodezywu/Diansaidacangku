6974e023e43da4415a9fbd167c4235bf1c8cce6a
YGX <3504594673@qq.com>
2026-07-25 01:12:00 +0800

    feat: add mspm0/MSPM0_LineFollow_JY61P/sensor.h

---
+++ mspm0/MSPM0_LineFollow_JY61P/sensor.h
@@ -0,0 +1,13 @@
+#ifndef __SENSOR_H
+#define __SENSOR_H
+
+#include <stdint.h>
+
+/* 璇诲彇鍗曡矾浼犳劅鍣ㄦ暟鎹嚱鏁板０鏄?*/
+uint8_t Sensor_Read_Single(uint8_t channel);
+
+/* 涓€娆¤鍙栧叓璺紶鎰熷櫒锛宺esult[0]锝瀝esult[7] 瀵瑰簲閫氶亾0锝?銆?*/
+void Sensor_Read_All(uint8_t result[8]);
+
+#endif /* __SENSOR_H */
+
